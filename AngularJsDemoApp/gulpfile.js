6e94a5a5ef2cb83c8470ca2f1ac38f48e5804063
FabianGosebrink <1oSzfcWKTk9raYjytkAT>
2015-09-06 17:30:05 +0200

    deleted dist and cleaning up gulp tasks; Added config.js for gulp

--- AngularJsDemoApp/gulpfile.js
+++ AngularJsDemoApp/gulpfile.js
@@ -1,61 +1,29 @@
-/// <vs BeforeBuild='default' SolutionOpened='watch' />
 var gulp = require("gulp");
 var jshint = require("gulp-jshint");
 var jscs = require("gulp-jscs");
-var uglify = require("gulp-uglify");
-var concat = require("gulp-concat");
 var del = require("del");
-var watch = require("gulp-watch");
 var inject = require('gulp-inject');
 
-var config = {
-    srcJSFiles: [
-        "./app/*.js",
-        "./app/*/*.js",
-        "./app/*/*/*.js",
-        "!./node_modules/**/*.js"
-    ],
-    targetHtmlFile: "index.html",
-    targetMinAppJsFile: "app.min.js",
-    targetDistFolder: "dist/",
-    targetDistJsFolder: "dist/js/",
-    root: "./"
-};
-
-gulp.task("watch", function(){
-    return gulp.watch(config.srcJSFiles, ["compress"]);
-});
-
-gulp.task("clean", function() {
-    del.sync([config.targetDistFolder + "**/*.*"]);
-});
+var config = require("./gulp.config")();
 
 gulp.task("vet", function() {
-    gulp.src(config.srcJSFiles)
+    return gulp.src(config.srcJSFiles)
         .pipe(jscs())
         .pipe(jshint())
         .pipe(jshint.reporter("jshint-stylish", { verbose: true }));
 });
 
-gulp.task("uglifyApp",["vet", "clean"], function() {
-  return gulp.src(config.srcJSFiles)
-    .pipe(concat(config.targetMinAppJsFile))
-    .pipe(uglify())
-    .pipe(gulp.dest(config.targetDistJsFolder));
-});
+gulp.task('injectJsIntoIndex', ["vet"], function () {
+    var wiredep = require('wiredep').stream;
+    var options = config.getWiredepDefaultOptions();
 
-gulp.task('serve-dist', ["uglifyApp"], function () {
-    var target = gulp.src(config.targetHtmlFile);
-    var sources = gulp.src(config.targetDistJsFolder + config.targetMinAppJsFile);
-
-    return target.pipe(inject(sources))
-        .pipe(gulp.dest(config.targetDistFolder));
-});
-
-gulp.task('serve-dev', ["vet", "clean"], function () {
-    var target = gulp.src(config.targetHtmlFile);
+    var target = gulp.src(config.targetIndexHtmlFile);
     var sources = gulp.src(config.srcJSFiles);
 
-    return target.pipe(inject(sources))
+    return target
+        .pipe(inject(sources, {
+            addRootSlash: false
+        }))
+        .pipe(wiredep(options))
         .pipe(gulp.dest(config.root));
 });
